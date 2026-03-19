549dddaa3c270877efd9082b11c58692bb3f35c3
Khunnith <khunnithmeng@gmail.com>
2026-03-19 16:03:08 +0700

    Implement map with marker of leaflet

--- app/src/app/pages/dashboard/dashboard.ts
+++ app/src/app/pages/dashboard/dashboard.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy } from '@angular/core';
+import { MapService } from '../../../../services/map-service';
 
 @Component({
   selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
+  standalone: true
 })
-export class Dashboard {}
+export class Dashboard implements AfterViewInit, OnDestroy {
+
+  constructor(private mapService: MapService) {}
+
+  ngAfterViewInit(): void {
+    this.mapService.initMap('map');
+
+    // Example data (replace with API later)
+    const mockData = this.generateMockData(2000);
+    this.mapService.renderMarkers(mockData);
+  }
+
+  ngOnDestroy(): void {
+    this.mapService.destroyMap();
+  }
+
+  private generateMockData(count: number) {
+    return Array.from({ length: count }).map(() => ({
+      lat: 11 + Math.random(),
+      lng: 104 + Math.random(),
+      type: 'vessel'
+    }));
+  }
+}
